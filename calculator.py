a6cdd3be7ab61161d9e1c9b85d05209bc80a55ef
Masnun Islam <masnun0410@outlook.com>
2024-05-02 14:47:26 +0100

    Updated calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,31 @@
-value1 = int(input("Enter the first number: "))
-value2 = int(input("Enter the second number: "))
-function = input("Enter the function: ")    
-if function == "+":
-    print(value1 + value2)  
-elif function == "-":
-    print(value1 - value2)
-elif function == "*":
-    print(value1 * value2)
-elif function == "/":
-    print(value1 / value2)
-else: 
-    print("Invalid function")
+import math
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)
+
+# Example usage
+print(add(5, 3))
+print(subtract(5, 3))
+print(multiply(5, 3))
+print(divide(5, 3))
+print(sin(math.pi/2))
+print(cos(math.pi))
+print(tan(math.pi/4))
